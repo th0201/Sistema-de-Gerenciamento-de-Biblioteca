3b52ddeff34a89e62ae21e9ebb29c44ac61f7ba5
MariaLuiza98 <142938147+MariaLuiza98@users.noreply.github.com>
2024-09-18 19:48:44 -0300

    Add files via upload

---
+++ Funcionario (1).java
@@ -0,0 +1,25 @@
+package projeto;
+
+class Funcionario extends Pessoa {
+    private boolean administrador;
+
+    public void cadastrarFuncionario() {
+        
+    }
+
+    public void editarFuncionario(){
+    }
+
+    public void buscarFuncionario(){
+    }
+
+    public void listarFuncionario(){
+    }
+
+    public void removerFuncionario(){
+    }
+
+    public void visualizarFuncionario() {
+        
+    }
+}
