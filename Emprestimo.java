7400e7bbb47adfcd9433e02115182cd23cf6a466
jeova paiva <108702370+jeovapaiva@users.noreply.github.com>
2024-09-18 19:38:33 -0300

    Add files via upload

---
+++ Emprestimo.java
@@ -0,0 +1,31 @@
+package projeto;
+
+class Emprestimo {
+    private Aluno aluno;
+    private Livro livro;
+    private String dataEmprestimo;
+    private String dataDevolucao;
+
+    public void cadastrarEmprestimo(Aluno aluno, Livro livro) {
+        
+
+    public void removerEmprestimo(){
+    }
+
+    public void editarEmprestimo(){
+    }
+
+    public void buscarEmprestimo(){
+    }
+
+    public void listarEmprestimos(){
+    }
+
+    public void visualizarEmprestimo() {
+        
+
+    public void listarEmprestimosAtrasados(){
+    }
+
+    
+}
