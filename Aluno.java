d4e34ae7cb713ba9a3d66d00a2f72e81dd6c76d7
Deuzimara Nunes <138496185+deuzimaranunes@users.noreply.github.com>
2024-10-02 21:04:26 -0300

    Add files via upload

--- Aluno.java
+++ Aluno.java
@@ -1,8 +1,17 @@
+package Projeto;
 
-class Aluno extends Pessoa {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Aluno extends Pessoa {
     private String matricula;
     private String curso;
-    
+    private List<Aluno> alunos = new ArrayList<>();
+
+    public List<Aluno> getAlunos() {
+        return alunos;
+    }
+
     public String getMatricula() {
         return matricula;
     }
@@ -23,56 +32,44 @@ class Aluno extends Pessoa {
         System.out.print("Digite o número do CPF: ");
         String cpf = ent.nextLine();
 
-        for (Pessoa p : pessoa) {
-            if(p.getCpf().equals(cpf)){
-                if(p instanceof Aluno){
-                    System.out.println("Aluno já cadastrado!");
-                    return;
-                }
-                System.out.println("Pessoa com CPF " + cpf + " já cadastrada.");
-                System.out.println("Deseja cadastrá-la como aluno? (sim/não)");
-                String resposta = ent.nextLine();
-
-                if (resposta.equalsIgnoreCase("sim")) {
-                    System.out.println("Cadastrar dados de: " + p.getNome());
-                    System.out.println("-----------------------------");
-                    System.out.println("PÁGINA DE CADASTRO DE ALUNOS");
-                    System.out.println("-----------------------------");
-
-                    System.out.print("MATRÍCULA:");
-                    matricula = ent.nextLine();
-                    System.out.print("CURSO:");
-                    curso = ent.nextLine();
-                    System.out.println("Aluno cadastrado!");
-                    return;
-                } else {
-                    System.out.println("Cadastro de aluno cancelado.");
-                    return;
-                }
+        for (Aluno a : alunos) {
+            if (a.getCpf().equals(cpf)) {
+                System.out.println("Aluno já cadastrado!");
+                return;
             }
-        }   
-        System.out.println("\nOlá! Parece que você ainda não completou seu cadastro. Para continuar, por favor, preencha suas informações. ");
-        super.cadastrarPessoa();
-        System.out.print("MATRÍCULA: ");
-        matricula = ent.nextLine();
-        System.out.print("CURSO: ");
-        curso = ent.nextLine();
-        System.out.println("Aluno cadastrado!");
+        }
+        System.out.println("Deseja se cadastrar como aluno? (sim/não)");
+        String resposta = ent.nextLine();
+
+        if (resposta.equalsIgnoreCase("sim")) {
+            System.out.println("-----------------------------");
+            System.out.println("PÁGINA DE CADASTRO DE ALUNOS");
+            System.out.println("-----------------------------");
+
+            Aluno novoAluno = new Aluno();
+            novoAluno.cadastrarPessoa();
+            System.out.print("MATRÍCULA:");
+            novoAluno.setMatricula(ent.nextLine());
+            System.out.print("CURSO:");
+            novoAluno.setCurso(ent.nextLine());
+            alunos.add(novoAluno);
+            System.out.println("Aluno cadastrado!");
+        } else {
+            System.out.println("Cadastro de aluno cancelado.");
+        }
     }
 
     public void editarAluno() {
-        System.out.println("Digite o número do CPF: ");
+        System.out.print("Digite o número do CPF: ");
         String cpf = ent.nextLine();
-
-        for (Pessoa p : pessoa) {
-            if(p instanceof Aluno && p.getCpf().equals(cpf)){
-                System.out.println("Editar dados do(a) aluno(a): " + p.getNome());
-                Aluno aluno = (Aluno) p;
-                super.editarPessoa();
+        for (Aluno a : alunos) {
+            if (a.getCpf().equals(cpf)) {
+                System.out.println("Editar dados do(a) aluno(a): " + a.getNome());
+                a.editarPessoa();
                 System.out.println("MATRÍCULA:");
-                aluno.setMatricula(ent.nextLine());
+                a.setMatricula(ent.nextLine());
                 System.out.println("CURSO:");
-                aluno.setCurso(ent.nextLine());
+                a.setCurso(ent.nextLine());
                 System.out.println("Dados editados com sucesso!");
                 return;
             }
@@ -81,35 +78,98 @@ class Aluno extends Pessoa {
     }
 
     public void buscarAluno() {
+        System.out.print("Digite o número do CPF: ");
+        String cpf = ent.nextLine();
+        boolean alunoEncontrado = false;
+
+        for (Aluno a : alunos) {
+            if (a.getCpf().equals(cpf)) {
+                alunoEncontrado = true;
+                System.out.println("NOME:" + a.getNome());
+                System.out.println("IDADE: " + a.getIdade());
+                System.out.println("CPF: " + a.getCpf());
+                System.out.println("EMAIL: " + a.getEmail());
+                System.out.println("MATRÍCULA: " + a.getMatricula());
+                System.out.println("CURSO: " + a.getCurso());
+                System.out.println("----------------------------------");
+                return;
+            }
+            if (!alunoEncontrado) {
+                System.out.println("Aluno não cadastrado!");
+            }
+        }
     }
 
     public void listarAlunos() {
-        if (pessoa.isEmpty()) {
+        if (alunos.isEmpty()) {
             System.out.println("Nenhum aluno cadastrado.");
         } else {
             System.out.println("\n-----------------------------");
             System.out.println("LISTA DE ALUNOS CADASTRADOS");
             System.out.println("-----------------------------");
 
-            for (Pessoa p : pessoa) {
-                if (p instanceof Aluno) {
-                    Aluno aluno = (Aluno) p;
-                    System.out.println("NOME: " + aluno.getNome());
-                    System.out.println("IDADE: " + aluno.getIdade());
-                    System.out.println("CPF: " + aluno.getCpf());
-                    System.out.println("EMAIL: " + aluno.getEmail());
-                    System.out.println("MATRÍCULA: " + aluno.getMatricula());
-                    System.out.println("CURSO: " + aluno.getCurso());
-                    System.out.println("----------------------------------");
-                }
+            for (Aluno a : alunos) {
+                System.out.println("NOME:" + a.getNome());
+                System.out.println("IDADE: " + a.getIdade());
+                System.out.println("CPF: " + a.getCpf());
+                System.out.println("EMAIL: " + a.getEmail());
+                System.out.println("MATRÍCULA: " + a.getMatricula());
+                System.out.println("CURSO: " + a.getCurso());
+                System.out.println("----------------------------------");
             }
-        }            
+        }
     }
 
     public void removerAluno() {
+        System.out.print("Digite o número do CPF: ");
+        String cpf = ent.nextLine();
+        boolean alunoEncontrado = false;
+
+        for (Aluno a : alunos) {
+            if (a.getCpf().equals(cpf)) {
+                alunoEncontrado = true;
+                System.out.println("Aluno(a): " + a.getNome());
+                System.out.println("Deseja remove-lo? (sim/não)");
+                String resposta = ent.nextLine();
+                if (resposta.equalsIgnoreCase("sim")) {
+                    alunos.remove(a);
+                    System.out.println("Aluno removido.");
+                    return;
+                } else {
+                    System.out.println("Remoção de aluno cancelada.");
+                }
+            }
+        }
+        if (!alunoEncontrado) {
+            System.out.println("Aluno não cadastrado!");
+        }
     }
 
-    public void visualizarAluno() {    
+    public void visualizarAluno() {
+        if (alunos.isEmpty()) {
+            System.out.println("Não há alunos para visualizar.");
+            return;
+        }
+        System.out.print("Digite o CPF do aluno que deseja visualizar: ");
+        String cpf = ent.nextLine();
+        boolean alunoEncontrado = false;
 
+        for (Aluno a : alunos) {
+            if (a.getCpf().equals(cpf)) {
+                alunoEncontrado = true;
+                System.out.println("Detalhes do Aluno: ");
+                System.out.println("NOME: " + a.getNome());
+                System.out.println("IDADE: " + a.getIdade());
+                System.out.println("CPF: " + a.getCpf());
+                System.out.println("EMAIL: " + a.getEmail());
+                System.out.println("MATRÍCULA: " + a.getMatricula());
+                System.out.println("CURSO: " + a.getCurso());
+                System.out.println("----------------------------------");
+                return;
+            }
+        }
+        if (!alunoEncontrado) {
+            System.out.println("CPF inválido.");
+        }
     }
-} 
+}
