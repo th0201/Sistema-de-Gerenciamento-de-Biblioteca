70fd40d97b5a86a81a0c43487f204a37683bd9b7
Thaynã Jácome <126775912+th0201@users.noreply.github.com>
2024-10-08 16:38:39 -0300

    Livro.java

--- Livro.java
+++ Livro.java
@@ -1,4 +1,6 @@
+package projeto;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Livro {
@@ -6,148 +8,200 @@ public class Livro {
     private String autor;
     private int ano;
     private int quantidadeDisponivel;
-    private boolean reservado;
+    private String corredor;
+    private String prateleira;
+    Scanner scanner = new Scanner(System.in);
+    protected static List<Livro> livro = new ArrayList<>();
 
-    private static ArrayList<Livro> livros = new ArrayList<>();
-
-    public Livro (String titulo, String autor, int ano, int quantidadeDisponivel) {
-        this.titulo = titulo;
-        this.autor = autor;
-        this.ano = ano;
-        this.quantidadeDisponivel = quantidadeDisponivel;
-    }
-
-    //get set
-
-    public String getTitulo (){
+    
+    public String getTitulo() {
         return titulo;
     }
 
-    public void setTilulo (String titulo) {
+    public void setTitulo(String titulo) {
         this.titulo = titulo;
     }
 
-    public String getAutor () {
+    public String getAutor() {
         return autor;
     }
 
-    public void setAutor (String autor) {
+    public void setAutor(String autor) {
         this.autor = autor;
     }
 
-    public int getAno () {
+    public int getAno() {
         return ano;
     }
 
-    public void setAno (int ano) {
+    public void setAno(int ano) {
         this.ano = ano;
     }
 
-    public int getQuantidadeDisponivel () {
-        return quantidadeDisponivel; 
+    public int getQuantidadeDisponivel() {
+        return quantidadeDisponivel;
     }
 
-    public void setQuantidadeDisponivel (int quantidadeDisponivel) {
+    public void setQuantidadeDisponivel(int quantidadeDisponivel) {
         this.quantidadeDisponivel = quantidadeDisponivel;
     }
 
-    public boolean isReservado() {
-        return reservado;
+    public String getCorredor() {
+        return corredor;
     }
 
-    public void setReservado(boolean reservado) {
-        this.reservado = reservado;
+    public void setCorredor(String corredor) {
+        this.corredor = corredor;
     }
 
-    
-     // Metodo para adicionar um livro à biblioteca
-     public void adicionarLivro() {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Digite o título do livro: ");
-        String titulo = scanner.nextLine();
-
-        System.out.print("Digite o autor do livro: ");
-        String autor = scanner.nextLine();
-
-        System.out.print("Digite o ano do livro: ");
-        int ano = scanner.nextInt();
-
-        System.out.print("Digite a quantidade disponível: ");
-        int quantidadeDisponivel = scanner.nextInt();
-
-        System.out.print("O livro está reservado? (true/false): ");
-        boolean reservado = scanner.nextBoolean();
-
-       
-        Livro novoLivro = new Livro(titulo, autor, ano, quantidadeDisponivel);
-
-        livros.add(novoLivro);
-
-        // Exibindo a confirmação
-        System.out.println("\nLivro adicionado com sucesso!");
-        System.out.println("Título: " + novoLivro.getTitulo());
-        System.out.println("Autor: " + novoLivro.getAutor());
-        System.out.println("Ano: " + novoLivro.getAno());
-        System.out.println("Quantidade Disponível: " + novoLivro.getQuantidadeDisponivel());
-        System.out.println("Reservado: " + (novoLivro.isReservado() ? "Sim" : "Não"));
+    public String getPrateleira() {
+        return prateleira;
     }
 
-    // Metodo para listar todos os livros da biblioteca
-    public void listarLivros() {
-        if (livros.isEmpty()) {
-            System.out.println("Não há livros cadastrados.");
+    public void setPrateleira(String prateleira) {
+        this.prateleira = prateleira;
+    }
+
+    public void cadastrarLivro() {
+        System.out.println("\n------------------------------");
+        System.out.println(" PÁGINA DE CADASTRO DE LIVROS");
+        System.out.println("------------------------------");
+        
+        System.out.print("TÍTULO: ");
+        this.titulo = scanner.nextLine();
+        System.out.print("AUTOR: ");
+        this.autor = scanner.nextLine();
+        System.out.print("ANO DA EDIÇÃO: ");
+        this.ano = scanner.nextInt();
+        System.out.print("QUANTIDADE DISPONÍVEL: ");
+        this.quantidadeDisponivel = scanner.nextInt();
+        scanner.nextLine();
+        System.out.print("CORREDOR: ");
+        this.corredor = scanner.nextLine();
+        System.out.print("PRATELEIRA: ");
+        this.prateleira = scanner.nextLine();
+        livro.add(this);
+    }
+
+    public void listarLivro() {
+        if (livro.isEmpty()) {
+            System.out.println("Nenhum livro cadastrado.");
         } else {
-            System.out.println("Livros cadastrados:");
-            for (Livro livro : livros) {
-                System.out.println("Título: " + livro.getTitulo() + ", Autor: " + livro.getAutor() +
-                                   ", Ano: " + livro.getAno() + ", Quantidade: " + livro.getQuantidadeDisponivel() +
-                                   ", Reservado: " + (livro.isReservado() ? "Sim" : "Não"));
+            System.out.println("\n-----------------------------");
+            System.out.println("LISTA DE LIVROS CADASTRADOS:");
+            System.out.println("-----------------------------");
+            for (int i = 0; i < livro.size(); i++) {
+                System.out.println("TÍTULO: " + livro.get(i).getTitulo());
+                System.out.println("AUTOR: " + livro.get(i).getAutor());
+                System.out.println("ANO DA EDIÇÃO: " + livro.get(i).getAno());
+                System.out.println("QUANTIDADE DISPONÍVEL: " + livro.get(i).getQuantidadeDisponivel());
+                System.out.println("CORREDOR: " + livro.get(i).getCorredor());
+                System.out.println("PRATELEIRA: " + livro.get(i).getPrateleira());
+                System.out.println("----------------------------------");
             }
         }
     }
 
-    // Metodo para remover um livro da biblioteca, levando em consideração se está reservado
-    public void removerLivro(String titulo, boolean reservado) {
-        if (!reservado) {
-            // Remover livro
-            System.out.println("Livro removido: " + titulo);
-        } else {
-            System.out.println("O livro " + titulo + " está reservado e não pode ser removido.");
-        }
-    }
-
-    // Metodo para editar as informações de um livro
     public void editarLivro() {
-       
-    }
-
-    // Metodo para buscar um livro
-    public void buscarLivro() {
-        
-       
-    }
-
-    // Metodo para visualizar os detalhes de um livro específico
-    public void visualizar() {
-        // Exibe os detalhes do livro
-        System.out.println("Título: " + this.titulo);
-        System.out.println("Autor: " + this.autor);
-        System.out.println("Ano: " + this.ano);
-        System.out.println("Quantidade Disponível: " + this.quantidadeDisponivel);
-    }
-
-    // Método para listar os livros que estão emprestados
-    public void listarLivrosEmprestados() {
-        // Lógica para listar livros que estão emprestados. fazer depois
-        System.out.println("Listando livros emprestados.");
-    }
-
-    // Método para listar os livros que estão fora de estoque
-    public void listarLivrosForaDeEstoque() {
-        if (quantidadeDisponivel == 0) {
-            System.out.println("Livro fora de estoque: " + this.titulo);
+        System.out.println("Digite o título do livro a ser editado: ");
+        String titulo = scanner.nextLine();
+        for (Livro l : livro) {
+            if (l.getTitulo().equals(titulo)) {
+                System.out.println("Editar o livro " + l.getTitulo());
+                System.out.println("TÍTULO: ");
+                l.titulo = scanner.nextLine();
+                System.out.println("AUTOR: ");
+                l.autor = scanner.nextLine();
+                System.out.println("ANO DA EDIÇÃO: ");
+                l.ano = scanner.nextInt();
+                System.out.println("QUANTIDADE DISPONÍVEL: ");
+                l.quantidadeDisponivel = scanner.nextInt();
+                scanner.nextLine();
+                System.out.println("CORREDOR: ");
+                l.corredor = scanner.nextLine();
+                System.out.println("PRATELEIRA: ");
+                l.prateleira = scanner.nextLine();
+                System.out.println("Livro editado com sucesso!");
+                return;
+            }
         }
+        System.out.println("Livro não encontrado!");
+
+    }
+
+    public void visualizarLivro() {
+        System.out.println("Digite o título do livro que deseja visualizar: ");
+        String titulo = scanner.nextLine();
+        for (Livro l : livro) {
+            if (l.getTitulo().equals(titulo)) {
+                System.out.println("TÍTULO: " + l.getTitulo());
+                System.out.println("AUTOR: " + l.getAutor());
+                System.out.println("ANO DA EDIÇÃO: " + l.getAno());
+                System.out.println("QUANTIDADE DISPONÍVEl: " + l.getQuantidadeDisponivel());
+                System.out.println("CORREDOR: " + l.getCorredor());
+                System.out.println("PRATELEIRA: " + l.getPrateleira());
+                System.out.println("----------------------------------");
+                return;
+            }
+        }
+        System.out.println("Livro não encontrado.");
+    }
+
+    public void buscarLivro() {
+        System.out.println("Digite o título do livro para buscar: ");
+        String titulo = scanner.nextLine();
+        for (Livro l : livro) {
+            if (l.getTitulo().equals(titulo)) {
+                System.out.println("TÍTULO: " + l.getTitulo());
+                System.out.println("AUTOR: " + l.getAutor());
+                System.out.println("ANO DA EDIÇÃO: " + l.getAno());
+                System.out.println("QUANTIDADE DISPONÍVEl: " + l.getQuantidadeDisponivel());
+                System.out.println("CORREDOR: " + l.getCorredor());
+                System.out.println("PRATELEIRA: " + l.getPrateleira());
+                System.out.println("----------------------------------");
+                return;
+            }
+        }
+        System.out.println("Livro não encontrado.");
+    }
+
+    public void removerLivro() {
+        System.out.println("Digite o título do livro a ser removido: ");
+        String titulo = scanner.nextLine();
+        for (int i = 0; i < livro.size(); i++) {
+            if (livro.get(i).getTitulo().equals(titulo)) {
+                livro.remove(i);
+                System.out.println("Livro removido com sucesso!");
+                return;
+            }
+        }
+        System.out.println("Livro não encontrado.");
+    }
+
+
+    public void listarLivrosForaDeEstoque(){
+        System.out.println("\nLIVROS FORA DE ESTOQUE:");
+        boolean encontrou = false;
+        for (Livro l : livro) {
+            if (l.getQuantidadeDisponivel() == 0) {
+                System.out.println("TÍTULO: " + l.getTitulo());
+                encontrou = true;
+            }
+        }
+        if (!encontrou) {
+            System.out.println("Nenhum livro está fora de estoque.");
+        }
+    }
+    
+    public String localizacaoDoLivro(){
+        System.out.println("Digite o título do livro para localizar: ");
+        String titulo = scanner.nextLine();
+        for (Livro l : livro) {
+            if (l.getTitulo().equals(titulo)) {
+                System.out.println("CORREDOR: " + l.getCorredor() + "\nPRATELEIRA: " + l.getPrateleira());
+            }
+        }
+        return("Livro não encontrado!");
     }
 
 }
